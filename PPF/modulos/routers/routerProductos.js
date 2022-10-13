4b1a5d50a01598246d51b55942784a5f53160925
ezelg19 <51120861+ezelg19@users.noreply.github.com>
2022-10-12 23:29:53 -0300

    PPT

--- PPF/modulos/routers/routerProductos.js
+++ PPF/modulos/routers/routerProductos.js
@@ -9,15 +9,9 @@ const admin = true
 router.get('/:id?', async (req, res) => {
     const id = parseInt(req.params.id)
     if (id) {
-        res.render('index',{
-            productosExist: await producto.getById(id),
-            productos: await producto.getById(id)
-            })
+        res.send(await producto.getById(id))
     } else {
-        res.render('index',{
-            productosExist: await producto.getAll(),
-            productos: await producto.getAll()
-        })
+        res.send(await producto.getAll())
     }
 })
 
@@ -25,7 +19,8 @@ router.post('/', async (req, res) => {
     if (admin) {
         const { nombre, descripcion, codigo, img, precio, stock } = req.body
         timestamp = new Date().toLocaleString()
-        const controlador = await producto.save({ timestamp: timestamp, nombre: nombre, descripcion: descripcion, codigo: codigo, img: 'https://cdn.icon-icons.com/icons2/2596/PNG/512/ad_product_icon_155850.png', precio: '$' + precio, stock: parseInt(stock) })
+        // const controlador = await producto.save({ timestamp: timestamp, nombre: nombre, descripcion: descripcion, codigo: codigo, img: 'https://cdn.icon-icons.com/icons2/2596/PNG/512/ad_product_icon_155850.png', precio: '$' + precio, stock: parseInt(stock) })
+        const controlador = await producto.save({ timestamp: timestamp, nombre: nombre, descripcion: descripcion, codigo: codigo, img: img, precio: '$' + precio, stock: parseInt(stock) })
         // if(controlador){res.status(400).send(controlador)}
         res.status(200).send('id: '+controlador)
     } else { res.status(500).send({ error: -1, descripcion: 'ruta "/appi/productos" metodo POST no autorizado' }) }
@@ -36,7 +31,8 @@ router.put('/:id', async (req, res) => {
         const id = parseInt(req.params.id)
         const { nombre, descripcion, codigo, img, precio, stock } = req.body
         timestamp = new Date().toLocaleString()
-        const controlador = await producto.actualizar({ id:id, timestamp:timestamp, nombre:nombre, descripcion:descripcion, codigo:codigo, img:'https://cdn.icon-icons.com/icons2/2596/PNG/512/ad_product_icon_155850.png', precio:'$'+precio, stock: parseInt(stock) })
+        // const controlador = await producto.actualizar({ id:id, timestamp:timestamp, nombre:nombre, descripcion:descripcion, codigo:codigo, img:'https://cdn.icon-icons.com/icons2/2596/PNG/512/ad_product_icon_155850.png', precio:'$'+precio, stock: parseInt(stock) })
+        const controlador = await producto.actualizar({ id:id, timestamp:timestamp, nombre:nombre, descripcion:descripcion, codigo:codigo, img:img, precio:'$'+precio, stock: parseInt(stock) })
         if(controlador){res.status(400).send(controlador)}
         else{res.sendStatus(200)}
     } else { res.status(500).send({ error: -1, descripcion: 'ruta "/appi/productos/:id" metodo PUT no autorizado' }) }
